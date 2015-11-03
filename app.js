7f3261fdf49609aeecad2f4709f67efb8bffba58
Rick Tesmond <tesmonrd@gmail.com>
2015-11-03 15:59:12 -0800

    Formatted table header and deleted excess elements

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var hours= ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 
-function cookieStore(storeLocation, minCustHr, maxCustHr, avePerCust, idName, totalId) {
+function cookieStore(storeLocation, minCustHr, maxCustHr, avePerCust) {
   this.storeLocation = storeLocation;
   this.minCustHr = minCustHr;
   this.maxCustHr = maxCustHr;
   this.avePerCust = avePerCust;
   this.total = 0;
   this.display = [];
-  this.idName = idName;
-  this.totalId = totalId;
 
   this.calcRand = function() {
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr +1)) + this.minCustHr;
@@ -23,7 +21,7 @@ function cookieStore(storeLocation, minCustHr, maxCustHr, avePerCust, idName, to
     for(var i = 0; i < hours.length; i++) {
       var totalThisHour = this.hourlyTot();
       this.total += totalThisHour;
-      this.display.push(hours[i] + ":" + " " + totalThisHour);
+      this.display.push(totalThisHour);
       console.log(totalThisHour);
     };
   };
@@ -48,21 +46,22 @@ function cookieStore(storeLocation, minCustHr, maxCustHr, avePerCust, idName, to
     };
 }
 
-var pikePlace = new cookieStore("Pike Place Market", 17, 88, 5.2, "pikeList", "pikeTotal");
-var seaTac = new cookieStore("SeaTac Airport", 6, 44, 1.2, "seaList", "seaTotal");
-var southCenter = new cookieStore("Southcenter Mall", 11, 38, 1.9, "southCenterList", "southCenterTotal");
-var bellevue = new cookieStore("Bellevue Square", 20, 48, 3.3, "bellevueList", "bellevueTotal");
-var alki = new cookieStore("Alki", 3, 24, 2.6, "alkiList", "alkiTotal");
+var pikePlace = new cookieStore("Pike Place Market", 17, 88, 5.2);
+var seaTac = new cookieStore("SeaTac Airport", 6, 44, 1.2);
+var southCenter = new cookieStore("Southcenter Mall", 11, 38, 1.9);
+var bellevue = new cookieStore("Bellevue Square", 20, 48, 3.3);
+var alki = new cookieStore("Alki", 3, 24, 2.6);
 
 var tbl = document.createElement("table");
 
 var topRow = function () {
-    var topEl = document.createElement('tr');
-      for(var i = 0; i < pikePlace.display.length; i++) {
-        var topTdEl = document.createElement("td");
+    var topEl = document.createElement('thead');
+    topEl.appendChild(document.createElement('td'));
+      for(var i = 0; i < hours.length; i++) {
+        var topThEl = document.createElement("td");
         var topHourDis = document.createTextNode(hours[i]);
-        topTdEl.appendChild(topHourDis);
-        topEl.appendChild(topTdEl);
+        topThEl.appendChild(topHourDis);
+        topEl.appendChild(topThEl);
       };
     tbl.appendChild(topEl);
 };
